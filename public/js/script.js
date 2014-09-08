edb46e710f63048dd114db204300ec28669eb777
Spencer Dixon <spencercdixon@gmail.com>
2014-09-08 14:58:33 -0400

    working firebase firepad and working form builder incrementor

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,7 @@
 $(function() {
 
   var $counter = 1
-  var $field =
-  "<div class='form-inline'>\
+  var $field = "<div class='form-inline'>\
     <div class='form-group'>\
       <label for='form_field_x'>Type of field</label>\
       <select class='form-control' id='form_field_x' name='form_fields[_x][type]'>\
@@ -13,18 +12,26 @@ $(function() {
       </select>\
       <label for='form_field_name_x'>Name Of First Form Field</label>\
       <input type='text' class='form-control' id='form_field_name_x' name='form_fields[_x][name]' placeholder='What field should be called'>\
-
     </div>\
   </div>";
 
-  $("#add_field").on("click", function(e){
+  $(".add_field").on("click", function(e){
     e.preventDefault();
   });
 
-  $("#add_field").click(function(){
+  $(".add_field").click(function(){
     var new_field = $field.replace(/_x/g, "_" + $counter );
     $(new_field).insertBefore("#placeholder");
     $counter += 1;
   });
 
+
 });
+
+var firepadDiv = document.getElementById('firepad-container');
+var firepadRef = new Firebase('https://form-builder.firebaseio.com/');
+var codeMirror = CodeMirror(firepadDiv, {
+      lineNumbers: true,
+      mode: 'javascript'
+    });
+var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror);
