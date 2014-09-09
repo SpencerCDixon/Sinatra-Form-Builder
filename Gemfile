4839a1d195465cd35dcb3ed9dd1b3b0f662ebeb2
Spencer Dixon <spencercdixon@gmail.com>
2014-09-09 11:34:31 -0400

    working finished product with some styling

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra'
-gem 'pry' if :development
+gem 'pry', group: :development
+gem 'rouge'
--- models/form_builder.rb
+++ models/form_builder.rb
@@ -1,5 +1,7 @@
+require 'rouge'
+
 class FormBuilder
-attr_accessor :path, :request, :fields, :field_name, :finished_fields, :inputs, :finished_form
+attr_accessor :path, :request, :fields, :field_name, :finished_fields, :inputs, :finished_form, :formatted_form
 
   def initialize(params_hash)
     @path = params_hash['path']
@@ -10,15 +12,16 @@ attr_accessor :path, :request, :fields, :field_name, :finished_fields, :inputs, 
     field_factory
 
     @finished_form = build_form
+    @formatted_form = formatter.format(lexer.lex(finished_form))
   end
 
   def build_form
       form = ""
-      form << "<form action='#{path}' method='#{request}'>"
+      form << "<form action='#{path}' method='#{request}'>\n"
       finished_fields.each do |field|
-        form << field
+        form << field + "\n"
       end
-      form << "<button type='submit'>Submit</button>"
+      form << "  <button type='submit'>Submit</button>\n"
       form << "</form>"
   end
 
@@ -26,14 +29,21 @@ attr_accessor :path, :request, :fields, :field_name, :finished_fields, :inputs, 
     inputs = []
     fields.each_value do |field|
       field.each do |key, value|
-        inputs << "#{key}='#{value}' "
+        inputs << "#{key}='#{value}'"
       end
-      complete = "<input " + inputs.join('') + ">"
+      complete = "  <input " + inputs.join('') + ">"
       finished_fields << complete
       inputs.clear
     end
   end
 
+  def formatter
+    @formatter ||= Rouge::Formatters::HTML.new
+  end
+
+  def lexer
+    @lexer ||= Rouge::Lexer.find_fancy("html", finished_form) || Rouge::Lexers::PlainText
+  end
   # def textarea_field
   # end
 
